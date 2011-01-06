6727238452254662560326613e1123644babd7ec
Honza Pokorny <me@honza.ca>
2011-01-05 22:45:22 -0400

    Figure out how to get id of dragged note.

--- stickies/media/notes.js
+++ stickies/media/notes.js
@@ -1,7 +1,15 @@
 // Notes
+var mle;
+
 $(function() {
 		$( ".column" ).sortable({
-			connectWith: ".column"
+			connectWith: ".column",
+		    stop: function(event, ui){
+		        // When done sorting, fire off an ajax call
+		        //$.post('/ajax', 
+		        mle = ui;
+		        var id = ui.item[0].id;
+		    }
 		});
  
 		$( ".portlet" ).addClass( "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" );
