c7a11dc3c1c7789fb4abec97520d601e929309fa
Honza Pokorny <me@honza.ca>
2011-01-08 20:22:14 -0400

    Prevent users from viewing projects they aren't a part of

--- stickies/notes/views.py
+++ stickies/notes/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse, Http404
+from django.contrib.auth.models import Group
+from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 from models import Note, Project
 from django.shortcuts import render_to_response, get_object_or_404
@@ -15,10 +17,15 @@ def index(request):
     return render_to_response('index.html', {'projects': projects},
             context_instance=RequestContext(request))
 
+@login_required
 def project(request, id):
     project = Project.objects.get(id=id)
+    group = get_object_or_404(Group, name=project.name)
+    if group not in request.user.groups.all():
+        raise Http404
     return render_to_response('project.html', {'project': project})
 
+@login_required
 def ajax(request):
     if not request.is_ajax():
         raise Http404
